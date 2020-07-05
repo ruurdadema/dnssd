e9d6f2c1604408f638e749b9d862afbde1ef61dd
Ruurd Adema <ruurdadema@gmail.com>
2020-07-05 13:57:23 +0200

    Made Browser a simple facade to BonjourBrowser

--- include/dnssd/Browser.h
+++ include/dnssd/Browser.h
@@ -6,7 +6,7 @@
 
 #include "ServiceDescription.h"
 #include "dnssd/common/Error.h"
-#include "dnssd/common/CommonBrowserInterface.h"
+#include "dnssd/bonjour/BonjourBrowser.h"
 
 #include <string>
 #include <thread>
@@ -14,16 +14,6 @@
 
 namespace dnssd {
 
-    class Browser
-    {
-    public:
-        using Listener = CommonBrowserInterface::Listener;
-
-        explicit Browser(const Listener& listener);
-        Error browseFor(const std::string& service);
-
-    private:
-        std::unique_ptr<CommonBrowserInterface> impl;
-    };
+    using Browser = BonjourBrowser;
 
 } // namespace dnssd
