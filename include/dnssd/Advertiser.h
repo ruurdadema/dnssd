cab6a76a51ab648ba5e1218a62ca46ffb672d53b
Ruurd Adema <ruurdadema@gmail.com>
2020-07-05 13:52:09 +0200

    Made Advertiser a simple facade

--- include/dnssd/Advertiser.h
+++ include/dnssd/Advertiser.h
@@ -1,30 +1,9 @@
 #pragma once
 
-#include <stdint.h>
-#include <string>
-#include <map>
-#include <functional>
-
-#include "dnssd/common/Error.h"
-#include "dnssd/common/CommonAdvertiserInterface.h"
-#include "dnssd/bonjour/TXTRecord.h"
+#include "dnssd/bonjour/BonjourAdvertiser.h"
 
 namespace dnssd {
 
-    class Advertiser
-    {
-    public:
-        using Listener = CommonAdvertiserInterface::Listener;
-
-        explicit Advertiser(const Listener& listener);
-        ~Advertiser();
-
-        Error registerService(const std::string& serviceName, uint16_t port) noexcept;
-        Error registerService(const std::string& serviceName, uint16_t port, const TXTRecord& txtRecord) noexcept;
-        Error registerService(const std::string& serviceName, uint16_t port, const std::map<std::string, std::string>& keysValues) noexcept;
-
-    private:
-        std::unique_ptr<CommonAdvertiserInterface> impl;
-    };
+    using Advertiser = BonjourAdvertiser;
 
 } // namespace dnssd
