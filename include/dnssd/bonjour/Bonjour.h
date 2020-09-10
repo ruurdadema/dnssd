5c444ed901d177e57dd7f9338cf9466fa12f7794
Ruurd Adema <ruurdadema@gmail.com>
2020-09-10 14:23:46 +0200

    Remove windows.h include warning-error
    It doesn't seem to be necessary

--- include/dnssd/bonjour/Bonjour.h
+++ include/dnssd/bonjour/Bonjour.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#ifdef _WINDOWS_
-#error "winsock2.h has to be included before windows.h please include this headerfile before the one including windows.h"
-#endif
-
 #ifdef _WIN32
 #define _WINSOCKAPI_ // Prevents inclusion of winsock.h in windows.h
 #include <winsock2.h>
